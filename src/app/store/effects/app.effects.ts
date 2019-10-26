c769928e0bec5627557ecf965f1159ab06b64ad0
Syam Pradeep <syampradeep2b6@gmail.com>
2019-10-26 11:26:39 +0530

    updated effect

--- src/app/store/effects/app.effects.ts
+++ src/app/store/effects/app.effects.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AppService } from 'src/app/app.service';
 import { fetchCustomers, fetchCustomerOdersSuccess, fetchCustomersSuccess, fetchCustomersFailure } from '../actions';
-import { map, mergeMap, catchError } from 'rxjs/operators';
+import { map, mergeMap, catchError, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 @Injectable()
 export class AppEffects {
   loadCustomers$ = createEffect(() => this.actions$.pipe(
     ofType(fetchCustomers),
-    mergeMap(() => this.appService.getCustomers()),
+    switchMap(() => this.appService.getCustomers()),
     map((res) => (fetchCustomersSuccess({
       payload: {
         customers: res
