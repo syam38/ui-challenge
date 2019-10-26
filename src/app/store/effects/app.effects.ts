baba1fa6b995b4e284b102031690245662cd0704
Syam Pradeep <syampradeep2b6@gmail.com>
2019-10-26 11:17:39 +0530

    added load customers effect

--- src/app/store/effects/app.effects.ts
+++ src/app/store/effects/app.effects.ts
@@ -1,7 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { AppService } from 'src/app/app.service';
+import { fetchCustomers, fetchCustomerOdersSuccess, fetchCustomersSuccess, fetchCustomersFailure } from '../actions';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable()
 export class AppEffects {
-  constructor(private actions$: Actions) {}
+  loadCustomers$ = createEffect(() => this.actions$.pipe(
+    ofType(fetchCustomers),
+    mergeMap(() => this.appService.getCustomers()),
+    map((res) => (fetchCustomersSuccess({
+      payload: {
+        customers: res
+      }
+    }))),
+    catchError((error) => {
+      return of(fetchCustomersFailure({
+        payload: {
+          error: error
+        }
+      }))
+    })
+  ));
+
+  constructor(
+    private actions$: Actions,
+    private appService: AppService
+  ) { }
 }
